dc567a183b8bd7b005d9525d9cb07de914d626ef
Talha_Altair <cooltalha27@gmail.com>
2022-08-06 19:30:58 +0530

    Updates for 2022-08-06 19:30:58

---
+++ app.js
@@ -0,0 +1,37 @@
+var aws     = require('aws-sdk');
+
+var email   = "cooltalha27@gmail.com";
+
+aws.config.loadFromPath(__dirname + '/config.json');
+var ses = new aws.SES();
+
+sender_text = 'talhaaaa'
+subject = 'subjekt'
+body = 'wassupp'
+
+var ses_mail = "From: " + sender_text + "<" + email + ">\n";
+ses_mail = ses_mail + "To: " + email + "\n";
+ses_mail = ses_mail + "Subject:"+ subject + "\n";
+ses_mail = ses_mail + "MIME-Version: 1.0\n";
+ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+ses_mail = ses_mail + "--NextPart\n";
+ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+ses_mail = ses_mail + body + "\n\n";
+ses_mail = ses_mail + "--NextPart\n";
+ses_mail = ses_mail + "Content-Type: text/plain;\n";
+ses_mail = ses_mail + "--NextPart--";
+
+var params = {
+    RawMessage: { Data: new Buffer(ses_mail) },
+    Destinations: [ email ],
+    Source: "<" + email + ">'"
+};
+
+ses.sendRawEmail(params, function(err, data) {
+    if(err) {
+        console.log(err);
+    } 
+    else {
+        console.log(data);
+    }           
+});
