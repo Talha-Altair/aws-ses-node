5bcf579c2297166a8b1e66cecd7454514d5805b9
Talha_Altair <cooltalha27@gmail.com>
2022-08-06 20:14:34 +0530

    Updates for 2022-08-06 20:14:34

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var aws     = require('aws-sdk');
 
-var email   = "cooltalha27@gmail.com";
+var email   = "altairtalha@gmail.com";
 
 aws.config.loadFromPath(__dirname + '/config.json');
 var ses = new aws.SES();
@@ -10,16 +10,16 @@ subject = 'subjekt'
 body = 'wassupp'
 
 var ses_mail = "From: " + sender_text + "<" + email + ">\n";
-ses_mail = ses_mail + "To: " + email + "\n";
-ses_mail = ses_mail + "Subject:"+ subject + "\n";
-ses_mail = ses_mail + "MIME-Version: 1.0\n";
-ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
-ses_mail = ses_mail + "--NextPart\n";
-ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
-ses_mail = ses_mail + body + "\n\n";
-ses_mail = ses_mail + "--NextPart\n";
-ses_mail = ses_mail + "Content-Type: text/plain;\n";
-ses_mail = ses_mail + "--NextPart--";
+ses_mail += "To: " + email + "\n";
+ses_mail += "Subject:"+ subject + "\n";
+ses_mail += "MIME-Version: 1.0\n";
+ses_mail += "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+ses_mail += "--NextPart\n";
+ses_mail += "Content-Type: text/html; charset=us-ascii\n\n";
+ses_mail += body + "\n\n";
+ses_mail += "--NextPart\n";
+ses_mail += "Content-Type: text/plain;\n";
+ses_mail += "--NextPart--";
 
 var params = {
     RawMessage: { Data: new Buffer(ses_mail) },
